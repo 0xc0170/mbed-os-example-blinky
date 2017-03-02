f6f706fa9f67492d3732e6cf623d3e8ab0c153a5
Martin Kojtal <c0170@rocketmail.com>
2017-03-02 11:41:26 +0000

    add update template files

--- mbed_config.h
+++ mbed_config.h
@@ -6,6 +6,7 @@
 
 // Configuration parameters
 #define MBED_CONF_LWIP_TCP_SERVER_MAX               4    // set by library:lwip
+#define MBED_CONF_LWIP_ADDR_TIMEOUT                 5    // set by library:lwip
 #define MBED_CONF_PLATFORM_DEFAULT_SERIAL_BAUD_RATE 9600 // set by library:platform
 #define MBED_CONF_LWIP_IPV4_ENABLED                 1    // set by library:lwip
 #define MBED_CONF_LWIP_TCP_SOCKET_MAX               4    // set by library:lwip
@@ -15,7 +16,7 @@
 #define MBED_CONF_PLATFORM_STDIO_FLUSH_AT_EXIT      1    // set by library:platform
 #define MBED_CONF_LWIP_UDP_SOCKET_MAX               4    // set by library:lwip
 #define MBED_CONF_NSAPI_PRESENT                     1    // set by library:nsapi
-#define MBED_CONF_LWIP_ADDR_TIMEOUT                 5    // set by library:lwip
+#define MBED_CONF_FILESYSTEM_PRESENT                1    // set by library:filesystem
 #define MBED_CONF_LWIP_IP_VER_PREF                  4    // set by library:lwip
 #define MBED_CONF_PLATFORM_STDIO_CONVERT_NEWLINES   0    // set by library:platform
 #define MBED_CONF_PLATFORM_STDIO_BAUD_RATE          9600 // set by library:platform
